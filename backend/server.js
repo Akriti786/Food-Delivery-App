91700bc8ee90ad4b65585beb83b3d2a6192e72b3
Akriti Shukla <akriti786shukla@gmail.com>
2026-09-02 14:31:43 +0530

    fd

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT,() => {
-    console.log(`Server running on the port ${PORT}`);
-  
-})
+app.get("/", (req, res) => {
+    res.json({
+        message: "Food Delivery API is running"
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
