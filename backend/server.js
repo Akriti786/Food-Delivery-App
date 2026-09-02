414a6c484037fae84079a8285885554781fe6714
Akriti Shukla <akriti786shukla@gmail.com>
2026-09-02 14:01:37 +0530

    first-comment

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT,() => {
+    console.log(`Server running on the port ${PORT}`);
+  
+})
